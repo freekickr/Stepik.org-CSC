89c4507429b2b6c30036adf2acf29699ef5ef5b7
Alex Tolkachev <freekickr@ya.ru>
2019-02-22 21:16:44 +0300

    added text analyzer lesson

--- src/main/java/basics/_3_OOP/TextAnalyzerApp.java
+++ src/main/java/basics/_3_OOP/TextAnalyzerApp.java
@@ -52,27 +52,48 @@ interface TextAnalyzer {
     Label processText(String text);
 }
 
-class SpamAnalyzer implements TextAnalyzer {
-    private String[] keywords;
+abstract class KeywordAnalyzer implements TextAnalyzer {
+    protected abstract String[] getKeywords();
+    protected abstract Label getLabel();
+
+    public Label processText(String text) {
+        for (String word : getKeywords())
+            if (text.contains(word))
+                return getLabel();
+        return Label.OK;
+    }
+}
+
+class SpamAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
+    private String[] keywords; //слова-спам
 
     public SpamAnalyzer(String[] keywords) {
         this.keywords = keywords;
     }
 
     @Override
-    public Label processText(String text) {
-        return null;
-    }
-}
-
-class NegativeTextAnalyzer implements TextAnalyzer {
-
-    public NegativeTextAnalyzer() {
+    protected String[] getKeywords() {
+        return keywords;
     }
 
     @Override
-    public Label processText(String text) {
-        return null;
+    protected Label getLabel() {
+        return Label.SPAM;
+    }
+}
+
+class NegativeTextAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
+
+    private final String[] KEYWORDS = {":(", "=(", ":|"};
+
+    @Override
+    protected String[] getKeywords() {
+        return KEYWORDS;
+    }
+
+    @Override
+    protected Label getLabel() {
+        return Label.NEGATIVE_TEXT;
     }
 }
 
@@ -85,12 +106,86 @@ class TooLongTextAnalyzer implements TextAnalyzer {
 
     @Override
     public Label processText(String text) {
-        return null;
+        //проверка на длинный комменты
+        if (text.length() > maxLength)
+            return Label.TOO_LONG;
+        else
+            return Label.OK;
     }
 }
 
 public class TextAnalyzerApp {
 
+    public Label checkLabels(TextAnalyzer[] analyzers, String text) {
+        for (TextAnalyzer comment : analyzers) {
+            Label label = comment.processText(text);
+            if (label != Label.OK)
+                return label;
+        }
+        return Label.OK;
+    }
+
+    public static void main(String[] args) {
+        // инициализация анализаторов для проверки в порядке данного набора анализаторов
+        String[] spamKeywords = {"spam", "bad"};
+        int commentMaxLength = 40;
+        TextAnalyzer[] textAnalyzers1 = {
+                new SpamAnalyzer(spamKeywords),
+                new NegativeTextAnalyzer(),
+                new TooLongTextAnalyzer(commentMaxLength)
+        };
+        TextAnalyzer[] textAnalyzers2 = {
+                new SpamAnalyzer(spamKeywords),
+                new TooLongTextAnalyzer(commentMaxLength),
+                new NegativeTextAnalyzer()
+        };
+        TextAnalyzer[] textAnalyzers3 = {
+                new TooLongTextAnalyzer(commentMaxLength),
+                new SpamAnalyzer(spamKeywords),
+                new NegativeTextAnalyzer()
+        };
+        TextAnalyzer[] textAnalyzers4 = {
+                new TooLongTextAnalyzer(commentMaxLength),
+                new NegativeTextAnalyzer(),
+                new SpamAnalyzer(spamKeywords)
+        };
+        TextAnalyzer[] textAnalyzers5 = {
+                new NegativeTextAnalyzer(),
+                new SpamAnalyzer(spamKeywords),
+                new TooLongTextAnalyzer(commentMaxLength)
+        };
+        TextAnalyzer[] textAnalyzers6 = {
+                new NegativeTextAnalyzer(),
+                new TooLongTextAnalyzer(commentMaxLength),
+                new SpamAnalyzer(spamKeywords)
+        };
+        // тестовые комментарии
+        String[] tests = new String[8];
+        tests[0] = "This comment is so good.";                            // OK
+        tests[1] = "This comment is so Loooooooooooooooooooooooooooong."; // TOO_LONG
+        tests[2] = "Very negative comment !!!!=(!!!!;";                   // NEGATIVE_TEXT
+        tests[3] = "Very BAAAAAAAAAAAAAAAAAAAAAAAAD comment with :|;";    // NEGATIVE_TEXT or TOO_LONG
+        tests[4] = "This comment is so bad....";                          // SPAM
+        tests[5] = "The comment is a spam, maybeeeeeeeeeeeeeeeeeeeeee!";  // SPAM or TOO_LONG
+        tests[6] = "Negative bad :( spam.";                               // SPAM or NEGATIVE_TEXT
+        tests[7] = "Very bad, very neg =(, very ..................";      // SPAM or NEGATIVE_TEXT or TOO_LONG
+        TextAnalyzer[][] textAnalyzers = {textAnalyzers1, textAnalyzers2, textAnalyzers3,
+                textAnalyzers4, textAnalyzers5, textAnalyzers6};
+        TextAnalyzerApp testObject = new TextAnalyzerApp();
+        int numberOfAnalyzer; // номер анализатора, указанный в идентификаторе textAnalyzers{№}
+        int numberOfTest = 0; // номер теста, который соответствует индексу тестовых комментариев
+        for (String test : tests) {
+            numberOfAnalyzer = 1;
+            System.out.print("test #" + numberOfTest + ": ");
+            System.out.println(test);
+            for (TextAnalyzer[] analyzers : textAnalyzers) {
+                System.out.print(numberOfAnalyzer + ": ");
+                System.out.println(testObject.checkLabels(analyzers, test));
+                numberOfAnalyzer++;
+            }
+            numberOfTest++;
+        }
+    }
 }
 
 
